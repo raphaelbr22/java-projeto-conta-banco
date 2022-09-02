9135eee12bc022216bbc8b0a27a5f963b7db9197
raphaelbr22 <raphael.fa.el@hotmail.com>
2022-09-01 22:33:19 -0300

    Finalização do projeto para envio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     /**
-     * @param args
-     * @throws Exception
+     * <h1>Conta Terminal</h1>
+     * Projeto Conta Terminal recebe os valores de entrada e retorna um texto simples com valores pré determinados
+     * <p>
+     * <b>Nota:</b> Projeto realizado para estudos e entrega do desafio do bootcamp "Formação Java Developer", 
+     * na plataforma DIO.
+     * 
+     * @author Raphael Barbosa Rocha
+     * @version 1.0
+     * @since 01/09/2022
+     * 
+     * @param numero esta é a variável de entrada para definir o número da conta
+     * @param agencia esta é a variável de entrada para definir o número da agência
+     * @param nomeCliente esta é a variável de entrada que vai armazenar o nome do cliente
+     * @param numero esta é a variável pré-definida com valor fixo e didatico para definir o saldo em conta
      */
     public static void main(String[] args) throws Exception {
        
